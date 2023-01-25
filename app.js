de71bd374d4de0604be590366abcd107f992b494
jxuho <jxuholee@gmail.com>
2023-01-25 12:58:08 +0900

    passport

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// const encrypt = require('mongoose-encryption');
-// const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// express-session
+app.use(
+  session({
+    secret: "long sentence", // env파일에 비밀로 저장될 내용
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// passport 사용하기 위해 init & passport가 session을 관리하도록 설정
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
@@ -21,9 +33,17 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+// mongoose schema에 passportLocalMongoose을 plugin으로 추가.(비밀번호를 hash & salt, db에 저장하는 역할)
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+// passport local configuration
+passport.use(User.createStrategy());
+
+// serialize, deserialize는 session에서 사용됨
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
   res.render("home");
@@ -37,36 +57,55 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
-app.post("/register", function (req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
-    newUser.save(function (err) {
-      if (!err) {
-        res.render("secrets");
-      } else {
-        console.log(err);
-      }
-    });
-  });
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
 });
 
-app.post("/login", function (req, res) {
-  const email = req.body.username;
-  const password = req.body.password;
-  User.findOne({ email: email }, function (err, result) {
+app.get("/logout", function (req, res) {
+  req.logout(function (err) {
     if (err) {
       console.log(err);
     } else {
-      if (result) {
-        bcrypt.compare(password, result.password, function (err, result) {
-          if (result === true) {
-            res.render("secrets");
-          }
+      res.redirect("/");
+    }
+  });
+});
+
+app.post("/register", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
         });
       }
+    }
+  );
+});
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  // passport가 제공하는 login() 사용.
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
     }
   });
 });
